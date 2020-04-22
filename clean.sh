cf40406c39d921b18f173754e0c81415dffb852d
ynn <yoann.maurel@irisa.fr>
2020-04-22 23:11:27 +0200

    change pass

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo docker rm -vf moodle
-sudo docker volume rm -vf moodle_data
-sudo docker volume rm -vf mariadb_data
+sudo docker volume rm -f moodle_data
+sudo docker volume rm -f mariadb_data
 sudo docker rm -vf mariadb
 sudo docker network rm moodle-tier
